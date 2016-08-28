086f09d5fcd7f1657d9c1e3744f5375f45cec508
Santyy <santhoshpdkt@hotmail.com>
2016-08-28 13:24:38 +0530

    Models added.

---
+++ MeMeSquad/src/MeMeSquad/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿namespace MeMeSquad.Models
+{
+    using Newtonsoft.Json;
+    using System;
+
+    public class Category
+    {
+        [JsonProperty(PropertyName = "id")]
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime Version { get; set; }
+    }
+}
