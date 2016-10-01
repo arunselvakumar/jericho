96c1abae15a570c7f876cd1ee091659ac211045f
arunselvakumar <b.arunselvakumar@gmail.com>
2016-10-01 18:01:45 +0530

    AuthTokenModel.cs Added.

---
+++ src/Jericho/Models/v1/AuthTokenModel.cs
@@ -0,0 +1,18 @@
+﻿namespace Jericho.Models.v1
+{
+    using System;
+
+    using Newtonsoft.Json;
+
+    public sealed class AuthTokenModel
+    {
+        [JsonProperty(PropertyName = "token")]
+        public string Token { get; set; }
+
+        [JsonProperty(PropertyName = "expires")]
+        public DateTime Expires { get; set; }
+
+        [JsonProperty(PropertyName = "issuedat")]
+        public DateTime Issued { get; set; }
+    }
+}
