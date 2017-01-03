1b774c75cf9e90b232ef4f961df0830ab8d47231
Santhosh <santhoshpdkt@hotmail.com>
2017-01-04 00:19:17 +0530

    ICommentAggregator.cs added

---
+++ src/Jericho/Aggregators/Interfaces/ICommentAggregator.cs
@@ -0,0 +1,13 @@
+﻿using Jericho.Models.v1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jericho.Aggregators.Interfaces
+{
+    interface ICommentAggregator
+    {
+        Task<CommentEntity> AggregateCommentForPost(PostEntity postEntity);
+    }
+}
